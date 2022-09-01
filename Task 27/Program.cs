c08c7b083d17a28b761cf3d1bce67d2ff20d7906
«Vitaliy <remotealex@yandex.ru>
2022-09-01 16:39:35 +0300

    Task 27

---
+++ Task 27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear ();
+Console.Write ("Введите число ");
+string? str = Console.ReadLine();
+char[] arr = str.ToCharArray();
+int result = 0;
+for (int C = 0; C < arr.Length; C++)
+{
+result = result + (arr [C] + 52 - 100);
+}
+Console.WriteLine(result);
